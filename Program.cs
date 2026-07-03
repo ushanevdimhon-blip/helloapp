5c24f922e4b7700bcb675aafb7c5573408c60033
black <black@example.com>
2026-07-03 17:18:34 +0500

    Change hello

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("hello everyone");
         }
 
         public static void GreetWhite()
