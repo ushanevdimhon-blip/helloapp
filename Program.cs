f89fbda7e7c539f0d13fc66df4358a10e4143f90
black <black@example.com>
2026-07-03 18:41:37 +0500

    Add calls

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("hello everyone");
+            GreetWhite();
+            GreetBlack();
         }
 
         public static void GreetWhite()
